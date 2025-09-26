4bb3495ae256d1184b085c29f405f583d697d70b
shriramjoshi7737 <shriramjoshi849@gmail.com>
2025-09-26 12:53:11 +0530

    add server url inside api

--- Feedback_Manangement_System/src/services/api.js
+++ Feedback_Manangement_System/src/services/api.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const Api = axios.create({
-  baseURL: "https://localhost:7056/api/",
+  //baseURL: "https://localhost:7056/api/",
+  baseURL:
+    "https://feedbackmanagementsystem-bcaygzc6bsekengt.centralindia-01.azurewebsites.net/api/",
 });
-
 
 export default Api;
