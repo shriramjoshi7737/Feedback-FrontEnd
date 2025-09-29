35b489be039fa88a801d972ee9875cceb64f1120
shriramjoshi7737 <shriramjoshi849@gmail.com>
2025-09-29 11:07:36 +0530

    add versal file in main project

--- Feedback_Manangement_System/src/services/api.js
+++ Feedback_Manangement_System/src/services/api.js
@@ -2,9 +2,8 @@ import axios from "axios";
 
 const Api = axios.create({
   //baseURL: "https://localhost:7056/api/",
-  // baseURL:
-  //   "https://feedbackmanagementsystem-bcaygzc6bsekengt.centralindia-01.azurewebsites.net/api/",
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL:
+    "https://feedbackmanagementsystem-bcaygzc6bsekengt.centralindia-01.azurewebsites.net/api/",
 });
 
 export default Api;
