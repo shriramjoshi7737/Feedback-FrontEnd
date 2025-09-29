bd8ed26a0b1976eb475f6247ad91b71f40632b65
shriramjoshi7737 <shriramjoshi849@gmail.com>
2025-09-29 10:36:46 +0530

    add env and envienamental variable

--- Feedback_Manangement_System/src/services/api.js
+++ Feedback_Manangement_System/src/services/api.js
@@ -2,8 +2,9 @@ import axios from "axios";
 
 const Api = axios.create({
   //baseURL: "https://localhost:7056/api/",
-  baseURL:
-    "https://feedbackmanagementsystem-bcaygzc6bsekengt.centralindia-01.azurewebsites.net/api/",
+  // baseURL:
+  //   "https://feedbackmanagementsystem-bcaygzc6bsekengt.centralindia-01.azurewebsites.net/api/",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
 export default Api;
